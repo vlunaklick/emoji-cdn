f08b0345014161aa0d93afa645f43df2ce76c398
Valentín Lunaklick <v.lunaklick@gmail.com>
2022-01-31 01:19:58 -0300

    feat: add styled-components

---
+++ frontend/pages/index.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+export default function Home() {
+	return (
+		<>
+			<Head>
+				<title>Emoji's CDN</title>
+				<meta name='description' content='CDN of emojis' />
+				<link rel='icon' href='/favicon.ico' />
+			</Head>
+
+			<main></main>
+		</>
+	)
+}

8f5959e784ae8c7e780d7165325081419e41354f
Valentín Lunaklick <v.lunaklick@gmail.com>
2022-02-01 17:22:45 -0300

    fix: add user-select to stlye

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "12a197f853c155c1"; }
+/******/ 		__webpack_require__.h = function() { return "48bda9e5dccb1701"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- frontend/pages/[emoji].js
+++ frontend/pages/[emoji].js
@@ -20,6 +20,7 @@ const EmojiWrapper = styled.main`
 
 	img {
 		-webkit-user-select: none;
+		user-select: none;
 		margin: auto;
 		background-color: hsl(0, 0%, 90%);
 		transition: background-color 300ms;

7a9f4e35320ae790bd990f9df5d299eadde1d85c
Valentín Lunaklick <v.lunaklick@gmail.com>
2022-02-04 12:33:52 -0300

    fix: error if emoji not avaible

--- controllers/emoji.controller.js
+++ controllers/emoji.controller.js
@@ -1,7 +1,6 @@
 import axios from 'axios'
 import pkg from 'xhr2'
 const { XMLHttpRequest } = pkg
-import path from 'node:path'
 
 class EmojiController {
 	serve = async (req, res) => {
@@ -9,24 +8,38 @@ class EmojiController {
 		let { emoji } = params
 		let { style } = query
 
+		let emojiREGEX =
+			/(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|\ud83c[\ude32-\ude3a]|\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])/g
+
+		if (emoji.match(emojiREGEX) === null) {
+			res.status(404).send('That icon isn&#39;t avaible')
+			return
+		}
+
 		if (style === undefined) {
 			style = 'apple'
 		}
 
-		let regexIMG = new RegExp(`<img.*(?:src|srcset)=\"(.*?${style}.*?)\">`, 'g')
+		let regexHTML = new RegExp(
+			`<img.*(?:src|srcset)=\"(.*?${style}.*?)\">`,
+			'g'
+		)
+
+		let regexURL =
+			/(?:(?:https?|ftp|file):\/\/|www\.|ftp\.)(?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|[-A-Z0-9+&@#\/%=~_|$?!:,.])*(?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|[A-Z0-9+&@#\/%=~_|$])/gim
 
 		axios.get(`https://emojipedia.org/${encodeURI(emoji)}`).then(({ data }) => {
-			if (data.match(regexIMG) === null) {
+			if (data.match(regexHTML) === null) {
 				res.status(404).send('Not avaible style of the icon')
+				return
 			} else {
-				let value = data.match(regexIMG)[0].replace('2x', '').trim()
-				let url = value.match(
-					/(?:(?:https?|ftp|file):\/\/|www\.|ftp\.)(?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|[-A-Z0-9+&@#\/%=~_|$?!:,.])*(?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|[A-Z0-9+&@#\/%=~_|$])/gim
-				)
-				fetchBlob(url[0], function (blob) {
-					let str = btoa(String.fromCharCode.apply(null, new Uint8Array(blob)))
+				let imgHTML = data.match(regexHTML)[0].replace('2x', '').trim()
+				let imgSRC = imgHTML.match(regexURL)
 
-					let imgFinal = Buffer.from(str, 'base64')
+				fetchBlob(imgSRC[0], function (blob) {
+					let src = btoa(String.fromCharCode.apply(null, new Uint8Array(blob)))
+
+					let imgFinal = Buffer.from(src, 'base64')
 
 					res.writeHead(200, {
 						'Content-Type': 'image/png',
